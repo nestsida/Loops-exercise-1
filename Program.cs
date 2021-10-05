6adc05b52344391b55eb1ba0b385d3655afc141c
sarah sida <nestsida@gmail.com>
2021-10-05 09:55:17 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Loops_loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var index = 0;
+            
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            
+           
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
